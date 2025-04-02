2b1559271db027cb2ad29459cf22b09ebe69e0ad
tofu0308 <isisaka3000@gmail.com>
2025-04-02 20:10:01 +0900

    feature componentを表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { HelloComponent } from './hello/hello.component'
 import { TodoComponent } from './todo/todo.component'
+import { FeatureComponent } from './feature/feature.component'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HelloComponent, TodoComponent],
+  imports: [RouterOutlet, HelloComponent, TodoComponent, FeatureComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
