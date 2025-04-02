7081cb699b1a9251a5bfecf0589263c01cb6c83a
tofu0308 <isisaka3000@gmail.com>
2025-04-02 20:05:12 +0900

    修正中

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { AppComponent } from '../app.component'
-import { FeatureModule } from '../feature/feature.module'
+import { AppComponent } from './app.component'
+import { FeatureModule } from './feature/feature.module'
 
 @NgModule({
   declarations: [AppComponent],
